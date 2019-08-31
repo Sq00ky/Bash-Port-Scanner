aef3ce66341f5478c3b2c62dc22aae75673c647f
Sq00ky <44957111+Sq00ky@users.noreply.github.com>
2019-08-31 02:12:39 -0400

    Update port_scanner.sh
    Ports now are displayed as they are live

--- port_scanner.sh
+++ port_scanner.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 #Variables
 empty=""
-rm /tmp/SpookyScan
+rm /tmp/SpookyScan > /dev/null 2>&1
+touch /tmp/SpookyScan
 #Args
 while [ "$1" != "" ]; do
-	case $1 in
-	-i | --ip )	shift;
-			ip=$1;
-	esac;
-	shift;
+	case "$1" in
+		-i | --ip )		ip="$2";	shift;;
+		-p | --ports )		ports="$2";	shift;;
+	esac
+	shift
 done
 #Checking if the -i is empty
 if [[ $ip == $empty ]]; then
 	echo "Please specify an IP address with -i"
 	exit
 fi
-for i in {1..80}; do
-	( echo >/dev/tcp/$ip/$i) > /dev/null 2>&1 && echo "Port" $i "is open" >> /tmp/SpookyScan;
+if [[ $ports == $empty ]]; then
+	echo "Please specify the max port range -p"
+	exit
+fi
+for i in $(seq 1 $ports); do
+	( echo > /dev/tcp/$ip/$i) > /dev/null 2>&1 && echo "Port" $i "is open";
 done
-cat /tmp/SpookyScan | grep "is open"
-rm /tmp/SpookyScan
