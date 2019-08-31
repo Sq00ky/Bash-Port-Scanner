bb1755aade3969c1f9e85c8e9431b493041a3d36
Sq00ky <44957111+Sq00ky@users.noreply.github.com>
2019-08-31 00:51:19 -0400

    Update port_scanner.sh

--- port_scanner.sh
+++ port_scanner.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 #Variables
 empty=""
+rm /tmp/SpookyScan
 #Args
 while [ "$1" != "" ]; do
-        case $1 in
-        -i | --ip )     shift;
-                        ip=$1;
-        esac;
-        shift;
+	case $1 in
+	-i | --ip )	shift;
+			ip=$1;
+	esac;
+	shift;
 done
 #Checking if the -i is empty
 if [[ $ip == $empty ]]; then
-        echo "Please specify an IP address with -i"
-        exit
+	echo "Please specify an IP address with -i"
+	exit
 fi
-#
 for i in {1..80}; do
-        telnet $ip $i;
+	( echo >/dev/tcp/$ip/$i) > /dev/null 2>&1 && echo "Port" $i "is open" >> /tmp/SpookyScan;
 done
+cat /tmp/SpookyScan | grep "is open"
+rm /tmp/SpookyScan
