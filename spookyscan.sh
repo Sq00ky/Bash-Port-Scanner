9ffc417137bca8bb3c0a18344ea981df796b9c88
Sq00ky <44957111+Sq00ky@users.noreply.github.com>
2019-08-31 03:04:15 -0400

    Update spookyscan.sh

--- spookyscan.sh
+++ spookyscan.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #Variables
 empty=""
-rm /tmp/SpookyScan > /dev/null 2>&1
-touch /tmp/SpookyScan
-#Args
+#Arguments for the script
 while [ "$1" != "" ]; do
 	case "$1" in
 		-i | --ip )		ip="$2";	shift;;
@@ -16,10 +14,12 @@ if [[ $ip == $empty ]]; then
 	echo "Please specify an IP address with -i"
 	exit
 fi
+#checking is -p is empty
 if [[ $ports == $empty ]]; then
 	echo "Please specify the max port range -p"
 	exit
 fi
+#Scans ports/Dumps closed ports/displays open ports
 for i in $(seq 1 $ports); do
 	( echo > /dev/tcp/$ip/$i) > /dev/null 2>&1 && echo "Port" $i "is open";
 done
